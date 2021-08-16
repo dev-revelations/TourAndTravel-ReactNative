2cae9aebfe3195d0a68aa18697cb530b61468c97
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-16 22:12:39 +0100

    Defining an interface for the theme

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,9 +1,16 @@
-import { colors } from './colors';
-import { fonts, fontSizes, fontWeights } from './fonts';
+import { colors, IColors } from './colors';
+import { fonts, fontSizes, fontWeights, IFonts, IFontSizeScreen, IFontWeights } from './fonts';
 
 export default {
     colors,
     fonts,
     fontSizes,
     fontWeights
+} as ITheme;
+
+export interface ITheme {
+    colors: IColors,
+    fonts: IFonts,
+    fontSizes: IFontSizeScreen,
+    fontWeights: IFontWeights
 }
