cb45e59c4ec36f1c954e7577951accac89de2582
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-16 21:18:34 +0100

    Adding custom fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { customFonts } from './src/utils';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [fontsLoaded, error] = useFonts(customFonts);
+
+  if (fontsLoaded && !error) {
+    return (
+      <View style={styles.container}>
+        <Text style={{fontFamily: 'Nunito', fontWeight:'700'}}>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    return (<AppLoading />);
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ src/utils/index.tsx
@@ -0,0 +1,22 @@
+
+import * as Font from 'expo-font';
+
+export const customFonts = {
+    'Nunito': require('../../assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-ExtraLight': {
+        uri: require('../../assets/fonts/Nunito-ExtraLight.ttf'),
+        display: Font.FontDisplay.FALLBACK
+    },
+    'Nunito-Light': {
+        uri: require('../../assets/fonts/Nunito-Light.ttf'),
+        display: Font.FontDisplay.FALLBACK
+    },
+    'Nunito-SemiBold': {
+        uri: require('../../assets/fonts/Nunito-SemiBold.ttf'),
+        display: Font.FontDisplay.FALLBACK
+    },
+    'Nunito-Bold': {
+        uri: require('../../assets/fonts/Nunito-Bold.ttf'),
+        display: Font.FontDisplay.FALLBACK
+    },
+};
