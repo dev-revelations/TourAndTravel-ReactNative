72d015ba2aca0679338a4c52d3b661062c25867b
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-23 08:36:38 +0100

    Implementing/using the Card component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import { View } from "react-native";
+import styled from "styled-components";
+import { IThemeProp } from "../../types";
+
+const defaultShadowRadius = 10;
+
+export const Card = styled(View) <CardProps>`
+    border-radius: 5px;
+    background-color: ${({ theme }) => theme.colors.background.primary};
+    shadow-color: #000;
+    shadow-offset: 0 10px;
+    shadow-opacity: 0.1;
+    shadow-radius: ${(props) => props.shadowRadius ? props.shadowRadius : defaultShadowRadius}px;
+    elevation: ${(props) => props.shadowRadius ? props.shadowRadius : defaultShadowRadius};
+`;
+
+interface CardProps extends IThemeProp {
+    shadowRadius?: number,
+}
