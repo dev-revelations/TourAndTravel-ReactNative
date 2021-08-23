72bd8402a8a0ed34b1cb7d903072be1831740c5c
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-23 12:22:22 +0100

    Enhancing TextBox to have on focus style

--- src/components/TextBox/TextBox.tsx
+++ src/components/TextBox/TextBox.tsx
@@ -1,10 +1,30 @@
+import React, { useState } from 'react';
+import { FC } from 'react';
+import { TextInputProps } from "react-native";
 import { TextInput } from "react-native";
 import styled from "styled-components";
 import { FontSizes, getFontSize, getSpace, Spaces } from "../../helpers/theme.helper";
+import { ITheme } from "../../types";
 
+const focusedStyle = (theme: ITheme) => `border-color: ${theme.colors.ui.primary};`;
+const bluredStyle = (theme: ITheme) => `border-color: rgba(0, 0, 0, 0.1);`;
 
-export const TextBox = styled(TextInput)`
+const StyledTextInput = styled(TextInput) <TextBoxProps>`
     font-family: ${({ theme }) => theme.fonts.body400};
     font-size: ${({ theme }) => getFontSize(theme, FontSizes.body)};
-    padding: ${({theme}) => getSpace(theme, Spaces.s2)};    
+    padding: ${({ theme }) => getSpace(theme, Spaces.s2)};
+    ${({ focused, theme }) => focused ? focusedStyle(theme) : bluredStyle(theme)}
 `;
+
+interface TextBoxProps extends TextInputProps {
+    focused?: boolean,
+}
+
+export const TextBox: FC<TextInputProps> = (props: TextInputProps) => {
+    const [isFocused, setIsFocused] = useState(false);
+    return (<StyledTextInput
+        focused={isFocused}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        {...props} />);
+}
