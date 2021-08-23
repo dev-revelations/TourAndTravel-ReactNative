d758cdc5f32b1be7b6f4a7db58e79bb26742fd49
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-23 10:57:35 +0100

    Fixing TextBox component padding

--- src/components/TextBox/TextBox.tsx
+++ src/components/TextBox/TextBox.tsx
@@ -1,10 +1,10 @@
 import { TextInput } from "react-native";
 import styled from "styled-components";
-import { FontSizes, getFontSize } from "../../helpers/theme.helper";
+import { FontSizes, getFontSize, getSpace, Spaces } from "../../helpers/theme.helper";
 
 
 export const TextBox = styled(TextInput)`
     font-family: ${({ theme }) => theme.fonts.body400};
     font-size: ${({ theme }) => getFontSize(theme, FontSizes.body)};
-    padding: 1%;
+    padding: ${({theme}) => getSpace(theme, Spaces.s2)};    
 `;
