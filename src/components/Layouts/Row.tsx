0ac7b43989e961cb799925b50956a40c8ba8def6
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-23 12:32:43 +0100

    Adding basic layout components

---
+++ src/components/Layouts/Row.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import styled from "styled-components";
+
+
+export const Row = styled(View)`
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
---
+++ src/components/Layouts/Screen.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+import styled from "styled-components";
+
+
+export const Screen = styled(View)`
+    padding: 3% 9%;
+`;
