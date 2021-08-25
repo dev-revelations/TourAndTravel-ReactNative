4fd4549d20f7447fff99bd4138f4f0b40fe8afcd
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-25 07:06:19 +0100

    Adding Col component to the layout components

---
+++ src/components/Layouts/Col.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components';
+
+export const Col = styled(View)`
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 350px;
+`;
