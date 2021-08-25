95a33f514c8f56930479671d8af9555bfa00e460
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-08-25 12:02:36 +0100

    Fixing Heading component sharp corners in iOS

--- src/components/Heading/Heading.style.tsx
+++ src/components/Heading/Heading.style.tsx
@@ -8,13 +8,15 @@ export const HeadingContainer = styled(View)`
     align-items: center;
     justify-content: center;
     padding: 2.5% 0;
+    overflow: hidden;
 `;
 
 export const HeadingSpan = styled(Text)`
     font-size: ${({ theme }) => getFontSize(theme, FontSizes.h5)};
     background-color: rgba(${({ theme }) => theme.colors.ui.primaryRgb},0.2);
     color: ${({ theme }) => theme.colors.ui.primary};
-    border-radius: 5px;
+    border-radius: 5px;    
     padding: 0.2% 2%;
     margin-left: 1%;
+    overflow: hidden;
 `;
