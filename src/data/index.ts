a08823515ba039e274a86ad073640a79381b4ec7
Behzad G.A <behzad.ghaffarnejad@gmail.com>
2021-09-09 13:27:15 +0100

    Moving video data to the data index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,1 +1,7 @@
-export { getVideos } from './video.data';
+export const getVideos = (): Videos => [
+    require('../../assets/images/vid-1.mp4'),
+    require('../../assets/images/vid-2.mp4'),
+    require('../../assets/images/vid-3.mp4'),
+    require('../../assets/images/vid-4.mp4'),
+    require('../../assets/images/vid-5.mp4'),
+];
--- src/data/video.data.ts
+++
@@ -1,8 +0,0 @@
-
-export const getVideos = (): Videos => [
-    require('../../assets/images/vid-1.mp4'),
-    require('../../assets/images/vid-2.mp4'),
-    require('../../assets/images/vid-3.mp4'),
-    require('../../assets/images/vid-4.mp4'),
-    require('../../assets/images/vid-5.mp4'),
-];
